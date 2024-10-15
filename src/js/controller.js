a8707f701da6a8a50a02a49cb2bb382aa1cdc59f
jackrichter <jackrichter1@gmail.com>
2024-10-16 01:16:31 +0200

    Fetching, and creating a recipe object.

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// Show a recipe
+const showRecipe = async function () {
+	try {
+		const res = await fetch(
+			"https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8355"
+			// "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+		);
+		const data = await res.json();
+
+		if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+		console.log(res, data);
+
+		// Create a recipe object
+		let { recipe } = data.data;
+		// let recipe = Object.assign({}, data.data.recipe);
+		recipe = {
+			id: recipe.id,
+			title: recipe.title,
+			publisher: recipe.publisher,
+			sourceUrl: recipe.source_url,
+			image: recipe.image_url,
+			servings: recipe.servings,
+			cookingTime: recipe.cooking_time,
+			ingredients: recipe.ingredients
+		};
+		console.log(recipe);
+	} catch (error) {
+		alert(error);
+	}
+};
+showRecipe();
