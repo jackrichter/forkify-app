6f6e2cbab0b04669b435a8df55cc6645765034c0
jackrichter <jackrichter1@gmail.com>
2024-10-24 01:48:47 +0200

    Cleanup of console logs.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -13,6 +13,7 @@ class RecipeView extends View {
 	}
 
 	_generateMarkup() {
+		console.log("In _generateMarkup (recipeView)");
 		return `
       <figure class="recipe__fig">
         <img src="${this._data.image}" alt="${this._data.title}" class="recipe__img" />
